bb886c9a25ed6007d35b6e1c5e22f0b2e39ad613
Irina Fomina <Irina.Paschenko@inforeachinc.com>
2018-10-03 10:20:25 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 1997-2013 InfoReach, Inc. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * InfoReach ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with InfoReach.
+ *
+ * CopyrightVersion 2.0
+ */
+
+/**
+ * TODO: add description
+ *
+ * @author Irina.Paschenko
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("finish");
+    }
+}
