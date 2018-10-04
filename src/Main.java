abbc660116422e3357044b341546f8f2953ebdf2
Irina Fomina <Irina.Paschenko@inforeachinc.com>
2018-10-04 10:21:00 +0300

    new branch

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@
 public class Main {
     public static void main(String[] args)
     {
+        // new dev brunch
         System.out.println("start");
         System.out.println("finish");
     }
